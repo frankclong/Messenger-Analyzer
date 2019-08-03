36d18159a4dce978f930a9c1be8c8a1a49dc60f1
Frank Long <fclong@uwaterloo.ca>
2019-08-02 22:21:00 -0600

    Revised main to utilize Contact class
    Added structure and methods to Contact class and updated statements in main

--- contact.py
+++ contact.py
@@ -1,10 +1,35 @@
 class Contact():
 	def __init__ (self, name):
 		self.name = name
+		#self.sent_msg_dates = []
+		#self.rcvd_msg_dates = []
 		self.msg_dates = []
-		# Consider splitting into sent_msg_dates and rcvd_msg_dates
+		self.msg_counts = []
 
 	# Add to list of date objects
 	# Perform calculations/analysis using this data
-	def add_msg_date(date):
-		msg_dates.append(date)
+	def add_sent_msg_date(self, msg_date):
+		msg_year = msg_date.year
+		if msg_year not in self.msg_dates:
+			self.msg_dates.append(msg_year)
+			self.msg_counts.append([0,1])
+		else:
+			pos = self.msg_dates.index(msg_year)
+			self.msg_counts[pos][1] += 1
+
+
+	def add_rcvd_msg_date(self, msg_date):
+		msg_year = msg_date.year
+		if msg_year not in self.msg_dates:
+			self.msg_dates.append(msg_year)
+			self.msg_counts.append([1,0])
+		else:
+			pos = self.msg_dates.index(msg_year)
+			self.msg_counts[pos][0] += 1
+
+
+	def get_dates(self):
+		return self.msg_dates
+
+	def get_counts(self):
+		return self.msg_counts
--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import os
 import datetime
 import csv
 import sys
+from contact import Contact
 
 _ROOT_PATH_ARG = 1
 
 def main():
-
+	print("hello")
 	rootpath = sys.argv[_ROOT_PATH_ARG]
 	filenames = os.listdir(rootpath) # get all files' and folders' names in the root directory
 
 	# Arrays that hold all data
 	name_list=[]
-	year_list = []
-	msg_count_list=[]
 
 	# [Name][Year][Month][Day][Messages sent, messages received]
 	# Print to CSV: Name, Year, Month, Day, Messages sent, Messages received
@@ -24,47 +23,38 @@ def main():
 		with open(rootpath + filename + "/message_1.json") as f:
 		  data = json.load(f)
 
+
 		# Check for group chat, "thread_type"
 		if data["thread_type"] == "Regular":
 			# 'title' is the name of contact
 			contact_name = data["title"]
-			name_list.append(contact_name)
+			name_list.append(Contact(contact_name))
 			c_num = c_num+ 1
+			my_contact=name_list[c_num]
+			#print(my_contact.name)
+
 			for i in data["messages"]:
 				sender = i["sender_name"]
-				ts = i["timestamp_ms"]
+				ts= i["timestamp_ms"]
 				dt_obj = datetime.datetime.fromtimestamp(int(ts/1000))
-				msg_year=dt_obj.year
-
-				# Start new list in year list
-				if len(year_list) <= c_num:	
-					year_list.append([msg_year])
-					msg_count_list.append([1])
-					year_num = 0
-				# If year list already exists
+				
+				if sender == my_contact.name:
+					my_contact.add_rcvd_msg_date(dt_obj)
 				else:
-					# Check if already exists
-					if msg_year not in year_list[c_num]:
-						year_list[c_num].append(msg_year)
-						msg_count_list[c_num].append(1)
-						year_num += 1
-					# If same year, add 1 
-					else:
-						msg_count_list[c_num][year_num] += 1		
+					my_contact.add_sent_msg_date(dt_obj)
 
 	with open('MessageData'+'.csv','w', newline='', encoding='utf-8-sig') as csvFile:
 		writer = csv.writer(csvFile)
-		hdr = ["Name","Year","Messages"]
+		hdr = ["Name","Year","Received Messages", "Sent Messages"]
 		writer.writerow(hdr)
 		nameIndex = 0
 		for name in name_list:
-			for year in year_list[nameIndex]:
-				yIndex = year_list[nameIndex].index(year)
-				# Only write to file data with over 100 messages 
-				if msg_count_list[nameIndex][yIndex] > 100:
-					row = [name, year, msg_count_list[nameIndex][yIndex]]
-					writer.writerow(row)
-			nameIndex += 1
+			msg_data = name.get_dates()
+			msg_counts = name.get_counts()
+			for entry in msg_data:
+				pos = msg_data.index(entry)
+				row = [name.name,entry, msg_counts[pos][0], msg_counts[pos][1]]
+				writer.writerow(row)
 
 if __name__ == "__main__":
 	main()
