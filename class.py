212e7d762bfbb0b50719bffd1c3c32f678d6c361
Frank Long <fclong@uwaterloo.ca>
2019-08-02 18:05:51 -0600

    Create contact class

---
+++ class.py
@@ -0,0 +1,10 @@
+class Contact():
+	def __init__ (self, name):
+		self.name = name
+		self.msg_dates = []
+		# Consider splitting into sent_msg_dates and rcvd_msg_dates
+
+	# Add to list of date objects
+	# Perform calculations/analysis using this data
+	def add_msg_date(date):
+		msg_dates.append(date)
