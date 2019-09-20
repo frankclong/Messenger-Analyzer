f6166214891905aae9a570a1024fad029514d5ff
Frank Long <fclong@uwaterloo.ca>
2019-09-20 19:06:40 +0200

    Added word count and additional filters
    Words sent/received each month now appears on the spreadsheet. Additional filter to ignore threads with less than 100 messages sent

--- contact.py
+++ contact.py
@@ -8,7 +8,7 @@ class Contact():
 
 	# Add to list of date objects
 	# Perform calculations/analysis using this data
-	def add_sent_msg_date(self, new_date):
+	def add_sent_msg_date(self, new_date, num_words):
 		msg_year = new_date.year
 		msg_month = new_date.month
 		pos = -1
@@ -16,22 +16,25 @@ class Contact():
 		# If array empty, just append
 		if len(self.msg_dates) == 0:
 			self.msg_dates.append(new_date)
-			self.msg_counts.append([0,1])
+			self.msg_counts.append([0,1,0,num_words])
 		else:
 			# Look for if month/year combo already exists
 			for ex_date in self.msg_dates:
 				if ex_date.year == msg_year and ex_date.month == msg_month:
 					pos = self.msg_dates.index(ex_date)
 					exit
-	
+			
+			# Exists
 			if pos != -1:
 				self.msg_counts[pos][1] += 1
+				self.msg_counts[pos][3] += num_words
+			# does not exist
 			else:
 				self.msg_dates.append(new_date)
-				self.msg_counts.append([0,1])
+				self.msg_counts.append([0,1,0,num_words])
 
 
-	def add_rcvd_msg_date(self, new_date):
+	def add_rcvd_msg_date(self, new_date, num_words):
 		msg_year = new_date.year
 		msg_month = new_date.month
 		pos = -1
@@ -39,7 +42,7 @@ class Contact():
 		# If array empty, just append
 		if len(self.msg_dates) == 0:
 			self.msg_dates.append(new_date)
-			self.msg_counts.append([1,0])
+			self.msg_counts.append([1,0, num_words,0])
 		else:
 			# Look for if month/year combo already exists
 			for ex_date in self.msg_dates:
@@ -49,9 +52,10 @@ class Contact():
 	
 			if pos != -1:
 				self.msg_counts[pos][0] += 1
+				self.msg_counts[pos][2] += num_words
 			else:
 				self.msg_dates.append(new_date)
-				self.msg_counts.append([1,0])
+				self.msg_counts.append([1,0,num_words,0])
 
 	def get_dates(self):
 		return self.msg_dates
--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import os
 import datetime
 import csv
 import sys
+import re
 from contact import Contact
 
 _ROOT_PATH_ARG = 1
 
 def main():
-	print("hello")
 	rootpath = sys.argv[_ROOT_PATH_ARG]
 	filenames = os.listdir(rootpath) # get all files' and folders' names in the root directory
 
 	# Arrays that hold all data
 	name_list=[]
 
-	# [Name][Year][Month][Day][Messages sent, messages received]
-	# Print to CSV: Name, Year, Month, Day, Messages sent, Messages received
+	# [Name][Year][Month][Day][Messages sent, messages received, words sent, words received]
+	# Print to CSV: Name, Year, Month, Day, Messages sent, Messages received, Words Sent, Words Received
 	c_num = -1
 	for filename in filenames:
 		# Name of file is "message_1.json"
@@ -24,8 +24,8 @@ def main():
 		  data = json.load(f)
 
 
-		# Check for group chat, "thread_type"
-		if data["thread_type"] == "Regular":
+		# Only analyze direct messages and if more than 100 messages sent
+		if data["thread_type"] == "Regular" and len(data["messages"]) > 100:
 			# 'title' is the name of contact
 			contact_name = data["title"]
 			name_list.append(Contact(contact_name))
@@ -34,18 +34,29 @@ def main():
 			#print(my_contact.name)
 
 			for i in data["messages"]:
-				sender = i["sender_name"]
-				ts= i["timestamp_ms"]
-				dt_obj = datetime.datetime.fromtimestamp(int(ts/1000))
-				
-				if sender == my_contact.name:
-					my_contact.add_rcvd_msg_date(dt_obj)
-				else:
-					my_contact.add_sent_msg_date(dt_obj)
+				# Check if content exists (not vid or photo)
+				if i.get("content"):
+					# Count number of words
+					num_words = 0
+					message_text = i["content"] # Message 
+					words = message_text.split() # Each "word" in array
+					for substr in words:
+						if re.search("[a-zA-Z0-9]", substr): # Count as "word" if contains letter or number
+							num_words += 1 
+
+					sender = i["sender_name"]
+					ts= i["timestamp_ms"]
+					dt_obj = datetime.datetime.fromtimestamp(int(ts/1000))
+					
+					if sender == my_contact.name:
+						my_contact.add_rcvd_msg_date(dt_obj, num_words)
+
+					else:
+						my_contact.add_sent_msg_date(dt_obj, num_words)
 
 	with open('MessageData'+'.csv','w', newline='', encoding='utf-8-sig') as csvFile:
 		writer = csv.writer(csvFile)
-		hdr = ["Name","Year","Month","Received Messages", "Sent Messages"]
+		hdr = ["Name","Year","Month","Received Messages", "Sent Messages","Received Words","Sent Words"]
 		writer.writerow(hdr)
 		nameIndex = 0
 		for name in name_list:
@@ -53,7 +64,7 @@ def main():
 			msg_counts = name.get_counts()
 			for entry in msg_data:
 				pos = msg_data.index(entry)
-				row = [name.name,entry.year,entry.month, msg_counts[pos][0], msg_counts[pos][1]]
+				row = [name.name,entry.year,entry.month, msg_counts[pos][0], msg_counts[pos][1],msg_counts[pos][2],msg_counts[pos][3]]
 				writer.writerow(row)
 
 if __name__ == "__main__":
