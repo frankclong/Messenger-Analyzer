b452283d1245b88a6902f096f43d5ebfa811c25d
Frank Long <fclong@uwaterloo.ca>
2024-06-23 19:13:28 -0400

    Contact and ConversationMessage models

--- messengeranalyzer/data_handler/models.py
+++ messengeranalyzer/data_handler/models.py
@@ -3,3 +3,20 @@ from django.db import models
 class MessagesDataUpload(models.Model):
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+class Contact(models.Model):
+    name = models.TextField()
+    folder_id = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+class ConversationMessage(models.Model):
+    sender_name = models.TextField()
+    content = models.TextField()
+
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    timestamp_ms = models.BigIntegerField()
+    sent_time = models.DateTimeField()
